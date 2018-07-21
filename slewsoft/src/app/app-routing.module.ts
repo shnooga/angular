4130146e4fbaef2f451ce83259938cc6bfecb216
papertrail <papertrail@hotmail.com>
2018-07-21 07:06:16 +0100

    Job search is now working

--- slewsoft/src/app/app-routing.module.ts
+++ slewsoft/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { JobsComponent } from './jobs/jobs.component';
 import {JobViewComponent} from './job-view/job-view.component';
 import {BoomCalcFormComponent} from './boom-calc-form/boom-calc-form.component';
+import {JobSearchComponent} from './job-search/job-search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'boomcalc', component: BoomCalcFormComponent },
+  { path: 'jobsearch', component: JobSearchComponent },
   { path: 'detail/:id', component: JobViewComponent },
   { path: 'jobs', component: JobsComponent }
 ];
