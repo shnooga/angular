5c7dd87e9c49580e210de30477824518b726b214
papertrail <papertrail@hotmail.com>
2018-07-23 07:22:38 +0100

    Fix boom angle calculation error

--- slewsoft/src/app/boom-calc-form/boom-calc-form.component.ts
+++ slewsoft/src/app/boom-calc-form/boom-calc-form.component.ts
@@ -13,36 +13,41 @@ function toTwoDecimal(num: number) { return num.toFixed(2); }
 
 export class BoomCalcFormComponent implements OnInit {
   private submitted = false;
-  model = new BoomCalc(3, 4, 5);
+  model = new BoomCalc(142, 30, 25);
   constructor() { }
 
   ngOnInit() {
     this.model.jibAngle = 0;
-    this.model.jibLength = 0;
+    this.model.jibLength = 10;
     this.model.insertQty = 0;
     this.model.insertLength = 0;
-    this.model.craneBaseHeight = 0;
+    this.model.craneBaseHeight = 10;
   }
 
   onSubmit() {
     this.submitted = true;
     const boomLength = this.model.minBoomLength;
     const bldOffset = this.model.buildingOffset;
-    const bldHeight = this.model.buildingHeight - this.model.craneBaseHeight;
-    const hypo1 = Math.sqrt(Math.pow(bldOffset, 2) + Math.pow(bldHeight, 2));
+    const adjBldHeight = this.model.buildingHeight - this.model.craneBaseHeight;
+    const hypo1 = this.calculateHypotenuse(bldOffset, adjBldHeight);
     // alert(hypo1);
-    this.model.boomAngleStr = toTwoDecimal(toDegree(Math.atan(bldHeight / bldOffset)));
     this.model.boomAngleStr = toTwoDecimal(this.extractBoomAngle());
-    this.model.objectOffsetStr = toTwoDecimal(bldOffset * (boomLength - hypo1) / hypo1);
-    this.model.overallRadiusStr = toTwoDecimal( bldOffset + Math.cos(toRadian(this.extractJibAngle())) * this.model.jibLength);
+    const objectOffSet = bldOffset * (boomLength - hypo1) / hypo1;
+    this.model.objectOffsetStr = toTwoDecimal(objectOffSet);
+    this.model.overallRadiusStr = toTwoDecimal( bldOffset
+      + objectOffSet
+      + Math.cos(toRadian(this.extractJibAngle())) * this.model.jibLength);
     this.model.overallBoomLengthStr = toTwoDecimal(this.model.minBoomLength + (this.model.insertQty * this.model.insertLength));
   }
   resetInput() {
     this.model = new BoomCalc(null, null, null);
   }
+  calculateHypotenuse(adjLen: number, oppositeLen: number) {
+    return Math.sqrt(Math.pow(adjLen, 2) + Math.pow(oppositeLen, 2));
+  }
 
   extractBoomAngle() {
-    return toDegree(Math.atan(this.model.buildingOffset / (this.model.buildingHeight - this.model.craneBaseHeight)));
+    return toDegree(Math.atan( (this.model.buildingHeight - this.model.craneBaseHeight) / this.model.buildingOffset));
   }
 
   extractJibAngle() {
@@ -50,5 +55,17 @@ export class BoomCalcFormComponent implements OnInit {
   }
 
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  get diagnostic() {
+    const bldOffset = this.model.buildingOffset;
+    const adjBldHeight = this.model.buildingHeight - this.model.craneBaseHeight;
+      // return JSON.stringify(this.model)
+    return 'adjBldHeight = bldHeight - craneBase = '  + adjBldHeight
+        + '  |   Boom Angle = atan(adjBldHeight/bldOffset) = ' + this.model.boomAngleStr
+        + '  |   hypotenuse = sqrt(adjBldHeight^2 + bldOffset^2) = ' + this.calculateHypotenuse(adjBldHeight, bldOffset)
+        + '  |   objectOffset = bldOffset * (boomLength - hypotenuse) / hypotenuse = ' + this.model.objectOffsetStr
+        + '  |   adjJibAngle = abs(boomAngle - jibOffsetAngle) = ' + this.extractJibAngle()
+        + '  |   jibLength: ' + this.model.jibLength
+        + '  |   jibOffsetLength = cos(adjJibAngle) * jibLength = ' + (Math.cos(toRadian(this.extractJibAngle())) * this.model.jibLength)
+        + '  |   Overall Radius = bldOffSet + objectOffset + jibOffsetLength = ' + this.model.overallRadiusStr;
+  }
 }
