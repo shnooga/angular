3ac1b215dcd50ae9fe7745232127692d56bd0873
Huey <shnooga@gmail.com>
2023-09-06 19:24:14 -0700

    Code clean up

--- lab-observable/src/app/customer/customer.component.ts
+++ lab-observable/src/app/customer/customer.component.ts
@@ -1,7 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {Customer} from "../model/customer"
 import {CustomerService} from "../services/customer.service";
-import {NgForm} from "@angular/forms";
+import {FormBuilder, FormGroup, NgForm} from "@angular/forms";
 import {AuthenticationService} from "../services/authentication.service";
 import {Observable} from "rxjs";
 import jwt_decode from 'jwt-decode';
@@ -15,20 +15,24 @@ import jwt_decode from 'jwt-decode';
 export class CustomerComponent {
   IsAddNew$: Observable<boolean>;
   Customers$: Observable<Array<Customer>>;
-  FirstName: string = "";
-  LastName: string = "";
-  @ViewChild('myForm') CustomerForm: NgForm;
+  // @ViewChild('myForm') CustomerForm: NgForm;
   LoginUserFirstName:string="DummyNane"
+  inputForm: FormGroup;
 
-  constructor(private custService : CustomerService, private authService : AuthenticationService) {
+  constructor(private fb: FormBuilder, private custService : CustomerService, private authService : AuthenticationService) {
     const decodedToken:any = jwt_decode(localStorage?.['token']);
     this.LoginUserFirstName = decodedToken.firstName;
     this.IsAddNew$ = this.custService.IsAddNew$;
     this.Customers$ = this.custService.Customers$;
+
+    this.inputForm = this.fb.group({
+      FName: '',
+      LName: ''
+    })
   }
 
   Save(){
-    this.custService.Save(new Customer(this.FirstName, this.LastName));
+    this.custService.Save(new Customer(this.inputForm.value.FName, this.inputForm.value.LName));
     this.reset();
   }
 
@@ -42,10 +46,22 @@ export class CustomerComponent {
   }
 
   reset() {
-    this.CustomerForm.reset();
+    this.inputForm.reset();
   }
 
   logout() {
     this.authService.logout();
+  }
+
+  FailsRequired(inputName: string): boolean {
+    let f = this.inputForm.get(inputName);
+    console.log(inputName + " failsMinChar: " + !(f?.dirty && f.errors?.['minlength']));
+    return !(f?.dirty && f.errors?.['required']);
+  }
+
+  FailsMinChar(inputName: string): boolean {
+    let f = this.inputForm.get(inputName);
+    console.log(inputName + " failsMinChar: " + !(f?.dirty && f.errors?.['minlength']) + " dirty: " + f?.dirty + " errors[minlength]: " + f?.errors?.['minLength']);
+    return !(f?.dirty && f.errors?.['minlength']);
   }
 }
